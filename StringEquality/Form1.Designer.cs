bb39020e960525ab0b6f502c93efa3da0571e08b
JadonF <36163291+JadonF@users.noreply.github.com>
2018-05-15 10:41:52 -0400

    Initial commit

---
+++ StringEquality/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace StringEquality
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.txtString1 = new System.Windows.Forms.TextBox();
+            this.txtString2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(13, 13);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(398, 13);
+            this.lblPrompt.TabIndex = 0;
+            this.lblPrompt.Text = "Enter 2 strings and click start to determine if they are the same (Not case-sensi" +
+    "tive).";
+            // 
+            // txtString1
+            // 
+            this.txtString1.Location = new System.Drawing.Point(13, 47);
+            this.txtString1.Name = "txtString1";
+            this.txtString1.Size = new System.Drawing.Size(100, 20);
+            this.txtString1.TabIndex = 1;
+            // 
+            // txtString2
+            // 
+            this.txtString2.Location = new System.Drawing.Point(298, 47);
+            this.txtString2.Name = "txtString2";
+            this.txtString2.Size = new System.Drawing.Size(100, 20);
+            this.txtString2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(164, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtString2);
+            this.Controls.Add(this.txtString1);
+            this.Controls.Add(this.lblPrompt);
+            this.Name = "Form1";
+            this.Text = "String Equality";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.TextBox txtString1;
+        private System.Windows.Forms.TextBox txtString2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ StringEquality/Form1.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Created by: Jadon F
+ * Created on: 15-May-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #33 - String Equality
+ * This program checks if 2 strings are equal.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringEquality
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool stringEqualityChecker (string string1, string string2)
+        {
+            //local variables
+            string upperS1;
+            string upperS2;
+            bool isEqual = false;
+
+            //define variables
+            upperS1 = string1.ToUpper();
+            upperS2 = string2.ToUpper();
+
+            //check
+            if (string1.Length == string2.Length)
+            {
+                if (upperS1 == upperS2)
+                {
+                    isEqual = true;
+                }
+            }
+            return isEqual;
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //local variables
+            string userString1;
+            string userString2;
+
+            //define variables.
+            userString1 = Convert.ToString(txtString1.Text);
+            userString2 = Convert.ToString(txtString2.Text);
+
+            //call function
+            bool isEqualMain = stringEqualityChecker(userString1, userString2);
+
+            //messagebox
+            if (isEqualMain == false)
+            {
+                MessageBox.Show("The two strings are NOT equal.", "String Equality");
+            }
+            if (isEqualMain == true)
+            {
+                MessageBox.Show("The two strings are equal", "String Equality");
+            }
+        }
+    }
+}
